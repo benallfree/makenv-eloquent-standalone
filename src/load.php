0beee0b68d6b271e199b6366cb51382f0cc91ce5
Ben Allfree <ben@benallfree.com>
2014-06-26 09:15:41 -0700

    Initial commit

---
+++ src/load.php
@@ -0,0 +1,19 @@
+<?php
+  
+use Illuminate\Database\Capsule\Manager as Capsule;
+
+$capsule = new Capsule;
+
+$capsule->addConnection(array(
+    'driver'    => DB_DRIVER,
+    'host'      => DB_HOST,
+    'database'  => DB_CATALOG,
+    'username'  => DB_USERNAME,
+    'password'  => DB_PASSWORD,
+    'charset'   => DB_CHARSET,
+    'collation' => DB_COLLATION,
+    'prefix'    => DB_PREFIX,
+));
+
+// Setup the Eloquent ORM... (optional; unless you've used setEventDispatcher())
+$capsule->bootEloquent();

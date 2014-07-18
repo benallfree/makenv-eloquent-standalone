540153092f40019fce0e9cf79c9df1e8a8bb829a
Ben Allfree <ben@benallfree.com>
2014-07-18 05:59:03 -0700

    Update init routine

--- src/load.php
+++ src/load.php
@@ -1,33 +1,40 @@
 <?php
-  
 use Illuminate\Database\Capsule\Manager as Capsule;
 
-Dotenv::required(array(
-  'DB_CONNECTION_NAME',
-  'DB_DRIVER',
-  'DB_HOST',
-  'DB_CATALOG',
-  'DB_USERNAME',
-  'DB_PASSWORD',
-  'DB_CHARSET',
-  'DB_COLLATION',
-  'DB_PREFIX',
-));
+class EloquentLoader
+{
+  static public function init()
+  {
 
-$capsule = new Capsule;
+    \Dotenv::required(array(
+      'DB_CONNECTION_NAME',
+      'DB_DRIVER',
+      'DB_HOST',
+      'DB_CATALOG',
+      'DB_USERNAME',
+      'DB_PASSWORD',
+      'DB_CHARSET',
+      'DB_COLLATION',
+      'DB_PREFIX',
+    ));
 
-$dcn = defined('DB_CONNECTION_NAME') ? $_ENV['DB_CONNECTION_NAME'] : 'default';
+    $capsule = new Capsule;
 
-$capsule->addConnection(array(
-    'driver'    => $_ENV['DB_DRIVER'],
-    'host'      => $_ENV['DB_HOST'],
-    'database'  => $_ENV['DB_CATALOG'],
-    'username'  => $_ENV['DB_USERNAME'],
-    'password'  => $_ENV['DB_PASSWORD'],
-    'charset'   => $_ENV['DB_CHARSET'],
-    'collation' => $_ENV['DB_COLLATION'],
-    'prefix'    => $_ENV['DB_PREFIX'],
-), $dcn);
+    $dcn = defined('DB_CONNECTION_NAME') ? $_ENV['DB_CONNECTION_NAME'] : 'default';
 
-// Setup the Eloquent ORM... (optional; unless you've used setEventDispatcher())
-$capsule->bootEloquent();
+    $capsule->addConnection(array(
+        'driver'    => $_ENV['DB_DRIVER'],
+        'host'      => $_ENV['DB_HOST'],
+        'database'  => $_ENV['DB_CATALOG'],
+        'username'  => $_ENV['DB_USERNAME'],
+        'password'  => $_ENV['DB_PASSWORD'],
+        'charset'   => $_ENV['DB_CHARSET'],
+        'collation' => $_ENV['DB_COLLATION'],
+        'prefix'    => $_ENV['DB_PREFIX'],
+    ), $dcn);
+
+    // Setup the Eloquent ORM... (optional; unless you've used setEventDispatcher())
+    $capsule->bootEloquent();
+  }
+}
+
